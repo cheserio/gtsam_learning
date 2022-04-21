d2281abe201dd4896ec9e3db0f6821fac5ab06ce
shiy <cheserio1997@gmail.com>
2022-04-21 10:04:42 +0200

    add chinese commit

--- examples/Pose2SLAMExample_g2o.cpp
+++ examples/Pose2SLAMExample_g2o.cpp
@@ -16,16 +16,18 @@
  * Syntax for the script is ./Pose2SLAMExample_g2o input.g2o output.g2o
  * @date May 15, 2014
  * @author Luca Carlone
+ * 2Dslam示例，从g2o读入数据，将其转换为因子图然后做优化，出入到一个文件中也是以g2o的形式
  */
 
-#include <gtsam/slam/dataset.h>
-#include <gtsam/geometry/Pose2.h>
-#include <gtsam/nonlinear/GaussNewtonOptimizer.h>
-#include <fstream>
+#include <gtsam/slam/dataset.h> // 读入数据
+#include <gtsam/geometry/Pose2.h> // 位姿表示
+#include <gtsam/nonlinear/GaussNewtonOptimizer.h> // 高斯牛顿优化器
+#include <fstream> // 基本的文件输出
 
 using namespace std;
 using namespace gtsam;
 
+// 用户根据自己情况调用该程序时需要修改inputFile outputFile 最大迭代次数 噪声鲁邦核函数类型选择
 // HOWTO: ./Pose2SLAMExample_g2o inputFile outputFile (maxIterations) (tukey/huber)
 int main(const int argc, const char *argv[]) {
   string kernelType = "none";
@@ -33,42 +35,49 @@ int main(const int argc, const char *argv[]) {
   string g2oFile = findExampleDataFile("noisyToyGraph.txt");  // default
 
   // Parse user's inputs
+  // 自己使用的时候会走这里上面是一些默认值
   if (argc > 1) {
-    g2oFile = argv[1];  // input dataset filename
+    g2oFile = argv[1]; // 输入数据集文件名
   }
   if (argc > 3) {
-    maxIterations = atoi(argv[3]);  // user can specify either tukey or huber
+    maxIterations = atoi(argv[3]); // 用户可以指定迭代次数
   }
   if (argc > 4) {
-    kernelType = argv[4];  // user can specify either tukey or huber
+    kernelType = argv[4]; // 用户可以指定 tukey 或 huber
   }
 
-  // reading file and creating factor graph
-  NonlinearFactorGraph::shared_ptr graph;
-  Values::shared_ptr initial;
-  bool is3D = false;
+  // 读取文件并创建因子图
+  NonlinearFactorGraph::shared_ptr graph; // 创建因子图智能指针
+  Values::shared_ptr initial; // 创建预测值容器指针
+  bool is3D = false; // 不是3D数据
+  // 如果损失函数啥也没输入
   if (kernelType.compare("none") == 0) {
+    // boost::tie是创建元组 后面的readG2o会返回因子图和初始预测值
     boost::tie(graph, initial) = readG2o(g2oFile, is3D);
   }
+  // 如果损失函数是huber
   if (kernelType.compare("huber") == 0) {
     std::cout << "Using robust kernel: huber " << std::endl;
     boost::tie(graph, initial) =
-        readG2o(g2oFile, is3D, KernelFunctionTypeHUBER);
+        readG2o(g2oFile, is3D, KernelFunctionTypeHUBER); // 使用HUBER鲁棒核函数来控制噪声 
   }
+  // 如果损失函数是tukey
   if (kernelType.compare("tukey") == 0) {
     std::cout << "Using robust kernel: tukey " << std::endl;
     boost::tie(graph, initial) =
-        readG2o(g2oFile, is3D, KernelFunctionTypeTUKEY);
+        readG2o(g2oFile, is3D, KernelFunctionTypeTUKEY); // 使用TUKEY鲁棒核函数来控制噪声 
   }
 
   // Add prior on the pose having index (key) = 0
+  // 这里是使用方差创建噪声模型
   auto priorModel =  //
       noiseModel::Diagonal::Variances(Vector3(1e-6, 1e-6, 1e-8));
-  graph->addPrior(0, Pose2(), priorModel);
+  graph->addPrior(0, Pose2(), priorModel); // 添加先验点 其余的点是通过g2o读入的
   std::cout << "Adding prior on pose 0 " << std::endl;
 
-  GaussNewtonParams params;
-  params.setVerbosity("TERMINATION");
+  GaussNewtonParams params; // 高斯牛顿优化器参数配置
+  params.setVerbosity("TERMINATION"); // 优化期间打印结果的详细程度 有个枚举的列表
+  // 载入迭代次数
   if (argc > 3) {
     params.maxIterations = maxIterations;
     std::cout << "User required to perform maximum  " << params.maxIterations
@@ -76,20 +85,22 @@ int main(const int argc, const char *argv[]) {
   }
 
   std::cout << "Optimizing the factor graph" << std::endl;
-  GaussNewtonOptimizer optimizer(*graph, *initial, params);
-  Values result = optimizer.optimize();
+  GaussNewtonOptimizer optimizer(*graph, *initial, params); // 创建迭代器由于之前使用的都是智能指针这里需要解引用
+  Values result = optimizer.optimize(); // 优化
   std::cout << "Optimization complete" << std::endl;
 
-  std::cout << "initial error=" << graph->error(*initial) << std::endl;
-  std::cout << "final error=" << graph->error(result) << std::endl;
+  std::cout << "initial error=" << graph->error(*initial) << std::endl; // 观测量和初始预测值之间的误差
+  std::cout << "final error=" << graph->error(result) << std::endl;  // 观测量和预测值之间的误差
 
   if (argc < 3) {
     result.print("result");
   } else {
+    // 输入参数大于3的话就存入了输出地址
     const string outputFile = argv[2];
     std::cout << "Writing results to file: " << outputFile << std::endl;
     NonlinearFactorGraph::shared_ptr graphNoKernel;
     Values::shared_ptr initial2;
+    // 将g2o中的文件再读一遍 然后结合上面得到的结果输出到输出文件中
     boost::tie(graphNoKernel, initial2) = readG2o(g2oFile);
     writeG2o(*graphNoKernel, result, outputFile);
     std::cout << "done! " << std::endl;
